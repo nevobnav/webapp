3baa709a84225022ceb4a59f2fca5c5e544bcc2e
kmvermeer <kazvermeer@gmail.com>
2019-03-21 17:11:56 +0100

    added lsit of known IPs

--- portal/signals.py
+++ portal/signals.py
@@ -19,20 +19,24 @@ def save_customer(sender, instance, **kwargs):
 
 
 # Track user logins according to https://stackoverflow.com/questions/37618473/how-can-i-log-both-successful-and-failed-login-and-logout-attempts-in-django
+known_IPs = { 'localhost':'127.0.0.1',
+              'TU office':'145.94.159.42',
+              }
+
 @receiver(user_logged_in)
 def user_logged_in_callback(sender, request, user, **kwargs):
     ip = request.META.get('REMOTE_ADDR')
-    if ip != '127.0.0.1':
+    if ip not in known_IPs.values():
         Logbook.objects.create(action='user_logged_in', ip=ip, username=user.username)
 
 @receiver(user_logged_out)
 def user_logged_out_callback(sender, request, user, **kwargs):
     ip = request.META.get('REMOTE_ADDR')
-    if ip != '127.0.0.1':
+    if ip not in known_IPs.values():
         Logbook.objects.create(action='user_logged_out', ip=ip, username=user.username)
 
 
 @receiver(user_login_failed)
 def user_login_failed_callback(sender, credentials, **kwargs):
-    if ip != '127.0.0.1':
+    if ip not in known_IPs.values():
         Logbook.objects.create(action='user_login_failed', username=credentials.get('username', None))
